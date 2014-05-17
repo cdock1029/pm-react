10e61ccd867231229b0ff9eb76d5e3c1c465f76f
Conor Dockry <conordockry@gmail.com>
2014-05-16 23:58:27 -0700

    add Header component

--- src/app.js
+++ src/app.js
@@ -281,6 +281,35 @@ var app = app || {};
         }
     });
 
+    var Header = React.createClass({
+        render: function() {
+            return (
+                <div className="navbar navbar-inverse navbar-fixed-top" role="navigation">
+                    <div className="container">
+                        <div className="navbar-header">
+                            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                                <span className="sr-only">Toggle navigation</span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                            </button>
+                            <a className="navbar-brand" href="#">Property Manager</a>
+                        </div>
+                        <div className="collapse navbar-collapse">
+                            <ul className="nav navbar-nav">
+                                <li className="active"><a href="#">Home</a></li>
+                                <li><a href="#/buildings">Buildings</a></li>
+                                <li><a href="#">Apartments</a></li>
+                                <li><a href="#/tenants">Tenants</a></li>
+                                <li><a href="#">Leases</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+    });
+
     /**
      * Renders appropriate components on page based on route.
      */
@@ -300,6 +329,7 @@ var app = app || {};
             case 'buildings':
                 return (
                     <div>
+                    <Header />
                     <DataGrid modelType={'Building'} dataColumns={['name', 'address', 'city', 'state', 'zip']} form={<NewBuildingForm />}/>
                     </div>
                 );
@@ -307,6 +337,7 @@ var app = app || {};
             case 'tenants':
                 return (
                     <div>
+                    <Header />
                     <DataGrid modelType={'Tenant'} dataColumns={['name', 'phone', 'email', 'balance']} form={<NewTenantForm />} />
                     </div>
                 );
